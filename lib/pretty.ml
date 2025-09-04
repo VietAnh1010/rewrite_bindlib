f621b08fb1d02de4dfdc911aa4dd22da0bb7397e
VietAnh1010 <vietanh101003@gmail.com>
2025-09-04 12:07:03 +0800

    Refine the ast

--- lib/pretty.ml
+++ lib/pretty.ml
@@ -2,31 +2,69 @@
 open Syntax
 open Bindlib
 
-let string_of_term_in (_ : ctxt) = function
-  | TVar x -> name_of x
-  | TTerm -> "<tterm>"
-  | TMetavar _ -> "<tmetavar>"
+let rec string_of_term_in (ctxt : ctxt) = function
+  | TVar x ->
+      name_of x
+  | TUnit ->
+      "()"
+  | TBool b ->
+      string_of_bool b
+  | TInt i ->
+      string_of_int i
+  | TLambda b ->
+      let b_str = string_of_staged_spec_binder_in ctxt b in
+      Format.sprintf "\\%s" b_str
+  | TMetavar _ ->
+      "<tmetavar>"
 
-let rec string_of_staged_spec_in (ctxt : ctxt) = function
+and string_of_state_in (_ctxt : ctxt) = function
+  | STState ->
+      "<ststate>"
+  | STMetavar _ ->
+      "<stmetavar>"
+
+and string_of_staged_spec_in (ctxt : ctxt) = function
   | Return t ->
       let t_str = string_of_term_in ctxt t in
-      Format.sprintf "ret %s" t_str
-  | Ensures _ ->
-      "ens <state>"
+      Format.sprintf "return %s" t_str
+  | Ensures st ->
+      let st_str = string_of_state_in ctxt st in
+      Format.sprintf "ensures %s" st_str
   | Bind (s, b) ->
       let s_str = string_of_staged_spec_in ctxt s in
-      let x, b, ctxt = unbind_in ctxt b in
-      let x_str = name_of x in
-      let b_str = string_of_staged_spec_in ctxt b in
-      Format.sprintf "(%s; %s. %s)" s_str x_str b_str
+      let b_str = string_of_staged_spec_binder_in ctxt b in
+      Format.sprintf "(%s; %s)" s_str b_str
   | Apply (f, t) ->
       let f_str = string_of_term_in ctxt f in
       let t_str = string_of_term_in ctxt t in
       Format.sprintf "%s %s" f_str t_str
-  | Disjunct _ -> assert false
-  | Exists _ -> assert false
-  | Reset _ -> assert false
-  | Shift _ -> assert false
-  | SMetavar _ -> assert false
+  | Disjunct (s1, s2) ->
+      let s1_str = string_of_staged_spec_in ctxt s1 in
+      let s2_str = string_of_staged_spec_in ctxt s2 in
+      Format.sprintf "(%s \\/ %s)" s1_str s2_str
+  | Exists b ->
+      let b_str = string_of_staged_spec_binder_in ctxt b in
+      Format.sprintf "(exists %s)" b_str
+  | Shift b ->
+      let b_str = string_of_staged_spec_binder_in ctxt b in
+      Format.sprintf "(shift %s)" b_str
+  | Reset s ->
+      let s_str = string_of_staged_spec_in ctxt s in
+      Format.sprintf "reset (%s)" s_str
+  | Dollar (s, cont) ->
+      let s_str = string_of_staged_spec_in ctxt s in
+      let cont_str = string_of_staged_spec_binder_in ctxt cont in
+      Format.sprintf "dollar (%s, %s)" s_str cont_str
+  | SMetavar _ ->
+      "<smetavar>"
+
+and string_of_staged_spec_binder_in (ctxt : ctxt) = function
+  | Binder b ->
+      let x, b, ctxt = unbind_in ctxt b in
+      let x_str = name_of x in
+      let b_str = string_of_staged_spec_in ctxt b in
+      Format.sprintf "%s. %s" x_str b_str
+  | SBMetavar _ ->
+      "<sbmetavar>"
 
 let string_of_staged_spec = string_of_staged_spec_in empty_ctxt
--- lib/syntax.ml
+++ lib/syntax.ml
@@ -5,33 +5,45 @@ type meta =
 
 type term =
   | TVar of term var
-  | TTerm
+  | TUnit
+  | TBool of bool
+  | TInt of int
+  | TLambda of staged_spec_binder
   | TMetavar of meta
 
-type state =
-  | State
+and state =
+  | STState
+  | STMetavar of meta
 
 and staged_spec =
   | Return of term
   | Ensures of state
-  | Bind of staged_spec * (term, staged_spec) binder
+  | Bind of staged_spec * staged_spec_binder
   | Apply of term * term
   | Disjunct of staged_spec * staged_spec
-  | Exists of (term, staged_spec) binder
+  | Exists of staged_spec_binder
   (* | Forall of (term, staged_spec) binder *)
-  | Reset of staged_spec * (term, staged_spec) binder
-  | Shift of (term, staged_spec) binder
-  (* | StagedVar of staged_spec var *)
+  | Shift of staged_spec_binder
+  | Reset of staged_spec
+  | Dollar of staged_spec * staged_spec_binder
   | SMetavar of meta
+
+and staged_spec_binder =
+  | Binder of (term, staged_spec) binder
+  | SBMetavar of meta
 
 module Constructors = struct
   let new_tvar = new_var (fun v -> TVar v)
 
   let mk_tvar = box_var
-  let mk_tterm = box TTerm
+  let mk_tunit = box TUnit
+  let mk_tbool = box_apply (fun b -> TBool b)
+  let mk_tint = box_apply (fun i -> TInt i)
+  let mk_tlambda = box_apply (fun b -> TLambda b)
   let mk_tmetavar = box_apply (fun m -> TMetavar m)
 
-  let mk_state = box State
+  let mk_state = box STState
+  let mk_stmetavar = box_apply (fun m -> STMetavar m)
 
   let mk_meta = box Meta
 
@@ -42,47 +54,61 @@ module Constructors = struct
   let mk_disjunct = box_apply2 (fun s1 s2 -> Disjunct (s1, s2))
   let mk_exists = box_apply (fun b -> Exists b)
   (* let forall = box_apply (fun b -> Forall b) *)
-  let mk_reset = box_apply2 (fun s cont -> Reset (s, cont))
   let mk_shift = box_apply (fun b -> Shift b)
+  let mk_reset = box_apply (fun s -> Reset s)
+  let mk_dollar = box_apply2 (fun s cont -> Dollar (s, cont))
   let mk_smetavar = box_apply (fun m -> SMetavar m)
+
+  let mk_binder = box_apply (fun b -> Binder b)
+  let mk_sbmetavar = box_apply (fun m -> SBMetavar m)
 
   let box_meta = function
     | Meta -> mk_meta
 
-  let box_term = function
+  let rec box_term = function
     | TVar x -> mk_tvar x
-    | TTerm -> mk_tterm
+    | TUnit -> mk_tunit
+    | TBool b -> mk_tbool (box b)
+    | TInt i -> mk_tint (box i)
+    | TLambda b -> mk_tlambda (box_staged_spec_binder b)
     | TMetavar m -> mk_tmetavar (box_meta m)
 
-  let box_state = function
-    | State -> mk_state
+  and box_state = function
+    | STState -> mk_state
+    | STMetavar m -> mk_stmetavar (box_meta m)
 
-  let rec box_staged_spec = function
+  and box_staged_spec = function
     | Return t -> mk_return (box_term t)
     | Ensures st -> mk_ensures (box_state st)
-    | Bind (s, b) -> mk_bind (box_staged_spec s) (box_binder box_staged_spec b)
+    | Bind (s, b) -> mk_bind (box_staged_spec s) (box_staged_spec_binder b)
     | Apply (f, t) -> mk_apply (box_term f) (box_term t)
     | Disjunct (s1, s2) -> mk_disjunct (box_staged_spec s1) (box_staged_spec s2)
-    | Exists b -> mk_exists (box_binder box_staged_spec b)
-    | Reset (s, cont) -> mk_reset (box_staged_spec s) (box_binder box_staged_spec cont)
-    | Shift b -> mk_shift (box_binder box_staged_spec b)
+    | Exists b -> mk_exists (box_staged_spec_binder b)
+    | Shift b -> mk_shift (box_staged_spec_binder b)
+    | Reset s -> mk_reset (box_staged_spec s)
+    | Dollar (s, cont) -> mk_dollar (box_staged_spec s) (box_staged_spec_binder cont)
     | SMetavar m -> mk_smetavar (box_meta m)
+
+  and box_staged_spec_binder = function
+    | Binder b -> mk_binder (box_binder box_staged_spec b)
+    | SBMetavar m -> mk_sbmetavar (box_meta m)
 end
 
 module Examples = struct
   open Constructors
 
-  (* representation of ret t; \f. ret t; \v. apply f(v) *)
   let ex1_box =
     let f = new_tvar "f" in
     let v = new_tvar "v" in
     mk_bind
-      (mk_return mk_tterm)
-      (bind_var f
-        (mk_bind
-          (mk_return mk_tterm)
-          (bind_var v
-            (mk_apply (mk_tvar f) (mk_tvar v)))))
+      (mk_return mk_tunit)
+      (mk_binder
+        (bind_var f
+          (mk_bind
+            (mk_return mk_tunit)
+            (mk_binder
+              (bind_var v
+              (mk_apply (mk_tvar f) (mk_tvar v)))))))
 
   let ex1 = unbox ex1_box
 end
